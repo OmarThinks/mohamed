87844e4621c5acb1ea336c14af6ce74028294e05
Omar Magdy <contact.parmag@gmail.com>
2021-04-06 20:20:52 +0200

    populate.py:organized records

--- django/cantiin/cantiin/populate.py
+++ django/cantiin/cantiin/populate.py
@@ -23,12 +23,18 @@ from django.contrib.auth.models import User
 def populate_users():
 	User.objects.using("testing").all().delete()
 
+	# 1
 	User.objects.using("testing").create(
-	username = "tewyute",email = "",password = "7895") # 1
-	User.objects.using("testing").create(username = "abc",
+	username = "user1",email = "",password = "7895") # 1
+	# 2
+	User.objects.using("testing").create(username = "user2",
 	email = "", password = "123786") # 2
-	User.objects.using("testing").create(username = "tyrhhbg",
+	# 3
+	User.objects.using("testing").create(username = "user3",
 	email ="",password = "tuasdghjkas") # 3
+
+
+
 
 
 def populate_products():
@@ -40,16 +46,23 @@ def populate_products():
 	u2 = all_users[1]
 	u3 = all_users[2]
 
+	
+	# 1
 	Product.objects.using("testing").create(name = "Android", 
 		price = 5, in_stock = True, owner = u1)	
+	# 2
 	Product.objects.using("testing").create(name = "Labtob", 
 		price = 100, in_stock = True, owner = u2)	
+	# 3
 	Product.objects.using("testing").create(name = "Computer", 
 		price = 60, in_stock = True, owner = u1)	
+	# 4
 	Product.objects.using("testing").create(name = "CPU", 
 		price = 3, in_stock = False, owner = u2)	
+	# 5
 	Product.objects.using("testing").create(name = "Suit", 
 		price = 40, in_stock = True, owner = u1)	
+	# 6
 	Product.objects.using("testing").create(name = "Meat", 
 		price = 78, in_stock = True, owner = u1)	
 
